44066b3247b98515df79891f8181c905fe7f1ff1
tyler <tylercharawell@gmail.com>
2021-07-15 08:41:34 -0700

    commit tree d43cc7564e5446aa3a7b789217574bb1db4905e3

--- app/application.rb
+++ app/application.rb
@@ -10,14 +10,20 @@ class Application
             item_name = req.path.split("/").last
             item = @@items.find{|i| i.name == item_name}
             if item
-                return [200, content_type, [item.price]]
+                # return [200, content_type, [item.price]]
+                resp.status = 200
+                resp.write(item.price)
             else
-                [400, content_type, ["Item not found"]]
+                # [400, content_type, ["Item not found"]]
+                resp.status = 400
+                resp.write("Item not found")
             end
         else
-            return [404, content_type, ["Route not found"]]
+            # return [404, content_type, ["Route not found"]]
+            resp.status = 404
+            resp.write("Route not found")
         end
-
+        resp.finish
         # return [status_code, content_type, data]
     end
 end
